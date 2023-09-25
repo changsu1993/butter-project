e950b0dd47e8c9af3eef78708779aa9686cd43d7
Changsu PARK <61610843+changsu1993@users.noreply.github.com>
2023-09-25 21:46:42 +0900

    feat: add default settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import App from './App.tsx';
+import GlobalStyles from '@styles/GlobalStyles.tsx';
+import theme from '@styles/theme.tsx';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </Router>
+  </QueryClientProvider>
+);
---
+++ src/styles/theme.tsx
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    DodgerBlue: '#18A0FB',
+    vibrantGreen: '#00C300',
+    lightRed: '#FF6633',
+  },
+};
+
+export default theme;
